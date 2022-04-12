1594f1772856b07fe6789839351f3830a216a246
Yana Ivanova <yana@plam.ch>
2022-04-12 15:42:53 +0300

    recursivly populate additional data to organizations

--- src/expressEndpoints.js
+++ src/expressEndpoints.js
@@ -65,8 +65,27 @@ export const generateEndpoints = () => {
     // organizations START
     createService('get', '/organizations', async (req, res) => {
         const organizations = await callApi({ req, path: 'organization/list/recursive' })
+        const allAdditionalData = await prisma.former22_organization.findMany()
 
-        res.json(organizations)
+        const populateAdditionalData = ({ orgsToPopulate }) =>
+            orgsToPopulate.map((currentOrg) => {
+                const currentOrgData = allAdditionalData.find(
+                    ({ organizationUuid }) => organizationUuid === currentOrg.id
+                )
+
+                const populatedChildren = populateAdditionalData({ orgsToPopulate: currentOrg.children })
+
+                if (currentOrgData) {
+                    // eslint-disable-next-line no-unused-vars
+                    const { _organizationUuid, _id, ...neededData } = currentOrgData
+
+                    return { ...currentOrg, ...neededData, children: populatedChildren }
+                } else {
+                    return { ...currentOrg, children: populatedChildren }
+                }
+            })
+
+        res.json(populateAdditionalData({ orgsToPopulate: organizations }))
     })
     // organizations END
 
