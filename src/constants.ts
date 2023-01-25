c0be2887a4019f1486c1f2dc10fc4befa1f15c7d
Daniel Rodriguez <maieutiquer@gmail.com>
2023-01-25 02:08:56 +0200

    add new status to enum

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,8 @@
 export const invoiceStatusesFromPrisma = {
     En_pr_paration: 'En préparation',
     A_traiter: 'A traiter',
+    Export_e: 'Exportée',
     Non_transmissible: 'Non transmissible',
     Annul_e: 'Annulée',
     Envoy_e: 'Envoyée',
-}
+} as const
