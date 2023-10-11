f1ecf939e0223dcced915963238315be6f82d9c5
firepolo <anthony.pasche@gmail.com>
2023-10-11 17:52:58 +0200

    refactor user & fix page users

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 
 import { prisma } from '..'
 import { createService, getLogDescriptions, LOG_TYPES } from '../utils'
-import { getProfessionFacetsValues, getUserProfession, parsePhoneForSms } from './inscriptionsUtils'
+import { parsePhoneForSms } from './inscriptionsUtils'
 
 export const usersRouter = Router()
 
@@ -10,81 +10,75 @@ createService(
     'get',
     '/',
     async (req, res) => {
-        const users = await prisma.former22_user.findMany({
+        const users = await prisma.claro_user.findMany({
             select: {
-                claro_user: {
-                    id: true,
-                    uuid: true,
-                    first_name: true,
-                    last_name: true,
-                    mail: true,
-                    claro_user_role: {
-                        select: {
-                            claro_role: {
-                                select: {
-                                    translation_key: true,
-                                },
-                            },
+                id: true,
+                uuid: true,
+                first_name: true,
+                last_name: true,
+                mail: true,
+                claro_field_facet_value: {
+                    select: {
+                        field_value: true,
+                    },
+                    where: {
+                        claro_field_facet: {
+                            name: { contains: 'FONCTION PROFESSIONNELLE' },
                         },
                     },
-                    user_organization: {
-                        where: {
-                            is_main: true,
-                        },
-                        select: {
-                            claro__organization: {
-                                select: {
-                                    name: true,
-                                },
-                            },
-                        },
-                    },
-                    phone: true,
                 },
+                claro_user_role: {
+                    select: {
+                        claro_role: {
+                            select: {
+                                translation_key: true,
+                            },
+                        },
+                    },
+                },
+                former22_user: {
+                    select: {
+                        shouldReceiveSms: true,
+                        colorCode: true,
+                        cfNumber: true,
+                    },
+                },
+                user_organization: {
+                    where: {
+                        is_main: true,
+                    },
+                    select: {
+                        claro__organization: {
+                            select: {
+                                name: true,
+                            },
+                        },
+                    },
+                },
+                phone: true,
             },
             where: {
-                claro_user: {
-                    is_removed: false,
-                },
+                is_removed: false,
             },
         })
 
-        const usersSettings = await prisma.former22_user.findMany()
-        const professionFacetsValues = await getProfessionFacetsValues()
-
-        const enrichedUsersData = users.map((current) => {
-            const currentUserSettings = usersSettings.find(({ userId }) => userId === current.uuid)
-            const currentUserProfession = getUserProfession({
-                userId: current.id,
-                professionFacetsValues,
-            })
-
-            let enrichedUser = {
-                id: current.uuid,
-                firstName: current.first_name,
-                lastName: current.last_name,
-                email: current.mail,
-                mainOrganizationName: current['user_organization'][0]?.['claro__organization'].name,
-                phone: current.phone,
-                phoneForSms: parsePhoneForSms({ phone: current.phone }),
-                roles: current.claro_user_role.map(({ claro_role: { translation_key } }) => translation_key),
-            }
-
-            if (currentUserSettings) {
-                // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                const { userId, ...settings } = currentUserSettings
-
-                enrichedUser = { ...enrichedUser, ...settings }
-            }
-
-            if (currentUserProfession) {
-                enrichedUser = { ...enrichedUser, profession: currentUserProfession }
-            }
-
-            return enrichedUser
-        })
-
-        res.json(enrichedUsersData)
+        return res.json(
+            users.map((user) => ({
+                id: user.uuid,
+                firstName: user.first_name,
+                lastName: user.last_name,
+                email: user.mail,
+                mainOrganizationName: user.user_organization[0]?.claro__organization.name,
+                phone: user.phone,
+                phoneForSms: parsePhoneForSms({ phone: user.phone }),
+                roles: user.claro_user_role.map(({ claro_role: { translation_key } }) => translation_key),
+                profession:
+                    user.claro_field_facet_value.length > 0
+                        ? JSON.parse(user.claro_field_facet_value[0].field_value).join(', ')
+                        : null,
+                ...user.former22_user,
+            }))
+        )
     },
     null,
     usersRouter
@@ -97,10 +91,18 @@ createService(
         const { userId } = req.params
         const { shouldReceiveSms, colorCode, cfNumber } = req.body
 
-        await prisma.former22_user.upsert({
-            where: { userId },
-            update: { shouldReceiveSms, colorCode, cfNumber },
-            create: { shouldReceiveSms, colorCode, cfNumber, userId },
+        await prisma.claro_user.update({
+            where: {
+                uuid: userId,
+            },
+            data: {
+                former22_user: {
+                    upsert: {
+                        create: { shouldReceiveSms, colorCode, cfNumber },
+                        update: { shouldReceiveSms, colorCode, cfNumber },
+                    },
+                },
+            },
         })
 
         res.json("L'utilisateur a été modifié")
