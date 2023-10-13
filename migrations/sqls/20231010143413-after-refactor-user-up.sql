2e78d1bae5928d07e76c1a6ebfb99dbaddc4f1b2
firepolo <anthony.pasche@gmail.com>
2023-10-13 11:53:26 +0200

    fix syntax error in sql code

--- migrations/sqls/20231010143413-after-refactor-user-up.sql
+++ migrations/sqls/20231010143413-after-refactor-user-up.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `former22_user`
 CHANGE COLUMN `userFk` `userId` INT(11) NOT NULL AFTER `cfNumber`,
 DROP COLUMN `userId`,
-ADD UNIQUE INDEX `userId` (`userId`);
+ADD UNIQUE INDEX `userId` (`userId`),
 ADD CONSTRAINT `FK_former22_user_claro_user` FOREIGN KEY (`userId`) REFERENCES `claro_user` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION;
