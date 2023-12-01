3642c67ed19b5bc8f0762bb06df0ea3343e3f0fa
firepolo <anthony.pasche@gmail.com>
2023-12-01 15:03:19 +0100

    fix contract generation

--- src/routes/contracts.js
+++ src/routes/contracts.js
@@ -83,83 +83,88 @@ createService(
                 })
             }
 
-            const subscriptions = await prisma.claro_cursusbundle_session_event_user.findMany({
-                where: {
-                    claro_user: {
-                        uuid: userId,
-                    },
-                    claro_cursusbundle_session_event: {
-                        claro_cursusbundle_course_session: {
-                            start_date: {
-                                gte: new Date(`${year}-01-01 00:00:00`),
-                                lt: new Date(`${year + 1}-01-01 00:00:00`),
+            const course = await prisma.claro_cursusbundle_course.findFirst({
+                select: {
+                    course_name: true,
+                    claro_cursusbundle_course_session: {
+                        select: {
+                            course_name: true,
+                            code: true,
+                            claro_cursusbundle_session_event: {
+                                select: {
+                                    uuid: true,
+                                    claro_planned_object: {
+                                        select: {
+                                            start_date: true,
+                                            end_date: true,
+                                            claro__location: {
+                                                select: {
+                                                    name: true,
+                                                },
+                                            },
+                                        },
+                                    },
+                                    former22_event: {
+                                        select: {
+                                            fees: true,
+                                        },
+                                    },
+                                },
                             },
-                            claro_cursusbundle_course: {
-                                uuid: courseId,
+                            claro_cursusbundle_course_session_user: {
+                                select: {
+                                    claro_user: {
+                                        select: {
+                                            first_name: true,
+                                            last_name: true,
+                                            claro_field_facet_value: {
+                                                where: {
+                                                    claro_field_facet: {
+                                                        type: 'choice',
+                                                        name: 'CIVILITÉ',
+                                                    },
+                                                },
+                                                select: {
+                                                    claro_field_facet: {
+                                                        select: {
+                                                            name: true,
+                                                            type: true,
+                                                        },
+                                                    },
+                                                    field_value: true,
+                                                },
+                                            },
+                                            user_organization: {
+                                                where: {
+                                                    is_main: true,
+                                                },
+                                                select: {
+                                                    claro__organization: {
+                                                        select: {
+                                                            name: true,
+                                                        },
+                                                    },
+                                                },
+                                            },
+                                        },
+                                    },
+                                },
                             },
                         },
                     },
                 },
-                select: {
-                    claro_cursusbundle_session_event: {
-                        select: {
-                            uuid: true,
-                            claro_planned_object: {
-                                select: {
-                                    start_date: true,
-                                    end_date: true,
-                                    claro__location: {
-                                        select: {
-                                            name: true,
-                                        },
-                                    },
-                                },
+                where: {
+                    uuid: courseId,
+                    claro_cursusbundle_course_session: {
+                        some: {
+                            start_date: {
+                                gte: new Date(`${year}-01-01 00:00:00`),
+                                lt: new Date(`${year + 1}-01-01 00:00:00`),
                             },
-                            claro_cursusbundle_course_session: {
-                                select: {
-                                    id: true,
-                                    code: true,
-                                    claro_cursusbundle_course: {
-                                        select: {
-                                            id: true,
-                                            course_name: true,
-                                        },
-                                    },
-                                },
-                            },
-                        },
-                    },
-                    claro_user: {
-                        select: {
-                            id: true,
-                            first_name: true,
-                            last_name: true,
-                            claro_field_facet_value: {
-                                where: {
-                                    claro_field_facet: {
-                                        type: 'choice',
-                                        name: 'CIVILITÉ',
-                                    },
-                                },
-                                select: {
-                                    claro_field_facet: {
-                                        select: {
-                                            name: true,
-                                            type: true,
-                                        },
-                                    },
-                                    field_value: true,
-                                },
-                            },
-                            user_organization: {
-                                where: {
-                                    is_main: true,
-                                },
-                                select: {
-                                    claro__organization: {
-                                        select: {
-                                            name: true,
-                                        },
+                            claro_cursusbundle_course_session_user: {
+                                some: {
+                                    claro_user: {
+                                        uuid: userId,
                                     },
                                 },
                             },
@@ -168,21 +173,14 @@ createService(
                 },
             })
 
-            const events = (
-                await prisma.former22_event.findMany({
-                    select: {
-                        eventId: true,
-                        fees: true,
-                    },
-                })
-            ).reduce((map, event) => map.set(event.eventId, event.fees), new Map())
+            const user =
+                course.claro_cursusbundle_course_session[0].claro_cursusbundle_course_session_user[0].claro_user
+            const events = course.claro_cursusbundle_course_session.reduce(
+                (a, s) => [...a, ...s.claro_cursusbundle_session_event],
+                []
+            )
 
-            const user = subscriptions[0].claro_user
-            const organization = user.user_organization[0]?.claro__organization.name || 'Indéterminé'
-            const civility = user.claro_field_facet_value[0]?.field_value.replaceAll('"', '') || 'Indéterminé'
-            const course =
-                subscriptions[0].claro_cursusbundle_session_event.claro_cursusbundle_course_session
-                    .claro_cursusbundle_course
+            console.log('PLANNED OBJECT', events[0])
 
             const content = fs.readFileSync(path.resolve(contractTemplateFilesDest, template.fileStoredName), 'binary')
             const zip = new PizZip(content)
@@ -194,50 +192,35 @@ createService(
             })
 
             doc.render({
-                FORMATEUR_CIVILITE: civility,
+                FORMATEUR_CIVILITE: user.claro_field_facet_value[0]?.field_value.replaceAll('"', '') || 'Indéterminé',
                 FORMATEUR_NOM: `${user.first_name} ${user.last_name}`,
-                FORMATEUR_ORGANISATION: organization,
+                FORMATEUR_ORGANISATION: user.user_organization[0]?.claro__organization.name || 'Indéterminé',
                 COURS_NOM: course.course_name,
-                SESSION_CODE: subscriptions.reduce(
-                    (array, subscription) => [
-                        ...array,
-                        array.indexOf(
-                            subscription.claro_cursusbundle_session_event.claro_cursusbundle_course_session.code
-                        ) >= 0
-                            ? ''
-                            : subscription.claro_cursusbundle_session_event.claro_cursusbundle_course_session.code,
-                    ],
-                    []
-                ),
-                SEANCE_DATE: subscriptions.map((subscription) =>
+                SESSION_CODE: course.claro_cursusbundle_course_session.map((s) => s.code),
+                SEANCE_DATE: events.map((e) =>
                     Intl.DateTimeFormat('fr-CH', {
                         timeZone: 'Europe/Zurich',
                         year: 'numeric',
                         month: '2-digit',
                         day: '2-digit',
-                    }).format(subscription.claro_cursusbundle_session_event.claro_planned_object.start_date)
+                    }).format(e.claro_planned_object.start_date)
                 ),
-                SEANCE_LIEU: subscriptions.map(
-                    (subscription) =>
-                        subscription.claro_cursusbundle_session_event.claro_planned_object?.claro__location?.name || ''
-                ),
-                SEANCE_HEURE_DEBUT: subscriptions.map((subscription) =>
+                SEANCE_LIEU: events.map((e) => e.claro_planned_object?.claro__location?.name || ''),
+                SEANCE_HEURE_DEBUT: events.map((e) =>
                     Intl.DateTimeFormat('fr-CH', {
                         timeZone: 'Europe/Zurich',
                         hour: '2-digit',
                         minute: '2-digit',
-                    }).format(subscription.claro_cursusbundle_session_event.claro_planned_object.start_date)
+                    }).format(e.claro_planned_object.start_date)
                 ),
-                SEANCE_HEURE_FIN: subscriptions.map((subscription) =>
+                SEANCE_HEURE_FIN: events.map((e) =>
                     Intl.DateTimeFormat('fr-CH', {
                         timeZone: 'Europe/Zurich',
                         hour: '2-digit',
                         minute: '2-digit',
-                    }).format(subscription.claro_cursusbundle_session_event.claro_planned_object.end_date)
+                    }).format(e.claro_planned_object.end_date)
                 ),
-                SEANCE_HONORAIRE: subscriptions.map((subscription) =>
-                    (events.get(subscription.claro_cursusbundle_session_event.uuid) || 0).toFixed(2)
-                ),
+                SEANCE_HONORAIRE: events.map((e) => (e.former22_event?.fees || 0).toFixed(2)),
             })
 
             const docxBuf = doc.getZip().generate({
