adb20535116c996ac792e6dd7787f0f3a2c54dd2
firepolo <anthony.pasche@gmail.com>
2023-12-12 10:01:55 +0100

    fix deleting event

--- migrations/sqls/20231201093626-add-foreign-to-event-up.sql
+++ migrations/sqls/20231201093626-add-foreign-to-event-up.sql
@@ -4,4 +4,4 @@ ALTER TABLE `former22_event`
 	DROP COLUMN `eventId`,
 	ADD PRIMARY KEY (`id`) USING BTREE,
 	ADD UNIQUE INDEX `event_id` (`event_id`),
-	ADD CONSTRAINT `fk_event_event` FOREIGN KEY (`event_id`) REFERENCES `claro_cursusbundle_session_event` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION;
+	ADD CONSTRAINT `fk_event_event` FOREIGN KEY (`event_id`) REFERENCES `claro_cursusbundle_session_event` (`id`) ON UPDATE NO ACTION ON DELETE CASCADE;
