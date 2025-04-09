26cfa0dc675431f9959033a110b0a191324bbfe5
firepolo <anthony.pasche@gmail.com>
2025-04-09 16:43:36 +0200

    add attestation generation

--- src/routes/attestations.js
+++ src/routes/attestations.js
@@ -4,6 +4,7 @@ import multer from 'multer'
 
 import { prisma } from '..'
 import { createService, LOG_TYPES, attestationTemplateFilesDest } from '../utils'
+import { generateAttestation } from '../helpers/attestations'
 
 const upload = multer({ dest: attestationTemplateFilesDest })
 
@@ -146,6 +147,146 @@ createService(
 
             return {}
         }
+    },
+    { entityType: LOG_TYPES.ATTESTATION },
+    attestationsRouter
+)
+
+createService(
+    'post',
+    '/generate',
+    async (req, res) => {
+        const { selectedAttestationTemplateUuid, uuids } = req.body
+
+        const inscriptions = await prisma.claro_cursusbundle_course_session_user.findMany({
+            select: {
+                id: true,
+                uuid: true,
+                validated: true,
+                confirmed: true,
+                registration_type: true,
+                status: true,
+                claro_cursusbundle_course_session: {
+                    select: {
+                        id: true,
+                        uuid: true,
+                        course_name: true,
+                        price: true,
+                        claro_cursusbundle_course: {
+                            select: {
+                                uuid: true,
+                                course_name: true,
+                                session_days: true,
+                                session_hours: true,
+                                generateInvoice: true,
+                                former22_course: {
+                                    select: {
+                                        goals: true,
+                                    },
+                                },
+                            },
+                        },
+                        claro_cursusbundle_course_session_user: {
+                            where: {
+                                registration_type: 'tutor',
+                            },
+                            select: {
+                                claro_user: {
+                                    select: {
+                                        first_name: true,
+                                        last_name: true,
+                                    },
+                                },
+                            },
+                        },
+                        claro_cursusbundle_session_event: {
+                            orderBy: {
+                                claro_planned_object: { start_date: 'asc' },
+                            },
+                            select: {
+                                claro_planned_object: {
+                                    select: {
+                                        start_date: true,
+                                        end_date: true,
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+                claro_user: {
+                    select: {
+                        id: true,
+                        uuid: true,
+                        mail: true,
+                        username: true,
+                        first_name: true,
+                        last_name: true,
+                        phone: true,
+                        claro_workspace_claro_user_workspace_idToclaro_workspace: {
+                            select: {
+                                id: true,
+                                uuid: true,
+                                slug: true,
+                                entity_name: true,
+                                code: true,
+                                claro_resource_node: true,
+                            },
+                        },
+                        user_organization: {
+                            where: {
+                                is_main: true,
+                            },
+                            select: {
+                                claro__organization: {
+                                    include: {
+                                        claro_cursusbundle_quota: {
+                                            select: {
+                                                id: true,
+                                            },
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+            where: {
+                uuid: {
+                    in: uuids,
+                },
+            },
+        })
+
+        for (const currentInscription of inscriptions) {
+            const user = currentInscription.claro_user
+            const {
+                course_name: sessionName,
+                claro_cursusbundle_course: {
+                    course_name: courseName,
+                    session_days: courseDurationDays,
+                    session_hours: courseDurationHours,
+                    former22_course,
+                },
+                claro_cursusbundle_course_session_user: tutors,
+                claro_cursusbundle_session_event: sessionDates,
+            } = currentInscription.claro_cursusbundle_course_session
+
+            await generateAttestation(selectedAttestationTemplateUuid, req, {
+                courseDurationDays,
+                courseDurationHours,
+                user,
+                courseName,
+                sessionName,
+                sessionDates,
+                former22_course,
+                tutors,
+                currentInscription,
+            })
+        }
+
+        res.json({ message: 'La génération à été effectuée avec succès' })
     },
     { entityType: LOG_TYPES.ATTESTATION },
     attestationsRouter
